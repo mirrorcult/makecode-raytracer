177117be7ff028eaed5fafe1d57461a0131f410a
Kara <lunarautomaton6@gmail.com>
2024-01-08 14:38:52 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,37 @@
+function vec3_refract (list: any[], list2: any[]) {
+	
+}
+function vec3_scalarmult (list: number[], num: number) {
+    return num * list[0] + (num * list[1] + num * list[2])
+}
+function vec3_sub (list: any[], list2: any[]) {
+	
+}
+function vec3_add (list: any[], list2: any[]) {
+	
+}
+function vec3_negate (list: any[]) {
+	
+}
+function vec3_dot (list: any[], list2: any[]) {
+	
+}
+function vec3_len (list: any[]) {
+	
+}
+function vec3_normalize (list: any[]) {
+	
+}
+function vec3_reflect (list: any[], list2: any[]) {
+	
+}
+function vec3_cross (list: number[], list2: number[]) {
+    a = list[0] * list2[0]
+    b = list[1] * list2[1]
+    c = list[2] * list2[2]
+    return a + (b + c)
+}
+let c = 0
+let b = 0
+let a = 0
+image.screenImage().setPixel(100, 100, 4)
